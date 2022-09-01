cc456ed25fde9ba2ac80c4760d731044b659f652
PhilippNeumann <Philippkd8@gmail.com>
2022-09-01 18:56:54 +0200

    started uml diagram
    uml setup for internal structure
    added primitve displacement implementation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from maya import cmds
 
 
@@ -6,6 +7,9 @@ class matBuilder(object):
     def __init__(self, name: str = None) -> None:
         self.shader = None
         self.shaderName = name
+        self.matSphere = None
+        self.sphereShape = None
+        self.shadingGroup = None
         self.shaderList = []
         self.diffTex = None
         self.metalTex = None
@@ -17,22 +21,39 @@ class matBuilder(object):
         self.matDict = {}  # Use dicitionary for material names based on folder names for keys and texture file paths as list for values
 
         # TODO get rid of hard coded paths
-        self.pathDiff = "/home/mainws/devkitBase/plug-ins/scripts/autoMat/textures/medieval_windows_29_73/medieval_windows_29_73_diffuse.jpg"
-        self.pathMetal = "/home/mainws/devkitBase/plug-ins/scripts/autoMat/textures/medieval_windows_29_73/medieval_windows_29_73_metalness.jpg"
-        self.pathRough = "/home/mainws/devkitBase/plug-ins/scripts/autoMat/textures/medieval_windows_29_73/medieval_windows_29_73_roughness.jpg"
-        self.pathNormal = "/home/mainws/devkitBase/plug-ins/scripts/autoMat/textures/medieval_windows_29_73/medieval_windows_29_73_normal.jpg"
-        self.pathDisplace = "/home/mainws/devkitBase/plug-ins/scripts/autoMat/textures/medieval_windows_29_73/medieval_windows_29_73_height.jpg"
+        self.pathDiff = "/mnt/Projects/tmp/autoMat_rnd/sourceimages/textures/medieval_windows_29_73/medieval_windows_29_73_diffuse.jpg"
+        self.pathMetal = "/mnt/Projects/tmp/autoMat_rnd/sourceimages/textures/medieval_windows_29_73/medieval_windows_29_73_metalness.jpg"
+        self.pathRough = "/mnt/Projects/tmp/autoMat_rnd/sourceimages/textures/medieval_windows_29_73/medieval_windows_29_73_roughness.jpg"
+        self.pathNormal = "/mnt/Projects/tmp/autoMat_rnd/sourceimages/textures/medieval_windows_29_73/medieval_windows_29_73_normal.jpg"
+        self.pathDisplace = "/mnt/Projects/tmp/autoMat_rnd/sourceimages/textures/medieval_windows_29_73/medieval_windows_29_73_height.jpg"
         # TODO add optional slots e.g. emissive, sss, opacity, transmission, coat, sheen
 
         self.files = [self.pathDiff, self.pathMetal,
                       self.pathRough, self.pathNormal, self.pathDisplace]
+
+    def createSphere(self):
+        self.matSphere, self.sphereShape = cmds.polyCube(name="PreviewSphere")
+        cmds.polySmooth(self.matSphere, divisions=3)
+        cmds.setAttr(self.sphereShape + '.aiSubdivType', 1)
+        cmds.setAttr(self.sphereShape +
+                     '.aiSubdivIterations', 3)  # TODO fix bug
+        cmds.delete(self.matSphere, constructionHistory=True)
 
     def createShader(self):
         self.shader = cmds.shadingNode(
             'aiStandardSurface', name=self.shaderName, asShader=True)
 
         self.shaderList.append(self.shader)
-        print(self.shaderList)
+
+    def assignShader(self):
+        # TODO is dis clever
+        self.shadingGroup = cmds.sets(name='PreviewSphereShadingGrp',
+                                      renderable=True, noSurfaceShader=True, empty=True)
+        cmds.connectAttr(self.shader + '.outColor',
+                         self.shadingGroup + '.surfaceShader', force=True)
+
+        cmds.select(self.matSphere, replace=True)
+        cmds.hyperShade(assign=self.shader)
 
     def getFiles(self):  # non active snippet
         self.files = cmds.fileDialog2(
@@ -45,6 +66,7 @@ class matBuilder(object):
         self.metalName = "metal_tex"
         self.roughName = "rough_tex"
         self.normalName = "normal_tex"
+        self.dispName = "disp_tex"
 
         # create texture nodes
         self.diffTex = cmds.shadingNode(
@@ -54,7 +76,9 @@ class matBuilder(object):
         self.roughTex = cmds.shadingNode(
             'aiImage', name=self.roughName, asTexture=True)  # aiImage Node only visible in lit vp
         self.normalTex = cmds.shadingNode(
-            'aiImage', name=self.normalName, asTexture=True)  # aiImage Node only visible in lit vp
+            'aiImage', name=self.normalName, asTexture=True)  # aiImage Node only visible in lit vp0
+        self.dispTex = cmds.shadingNode(
+            'aiImage', name=self.dispName, asTexture=True)  # aiImage Node only visible in lit vp
         # TODO test aiImage with UDIM
         # TODO add option to use default file node
 
@@ -95,7 +119,7 @@ class matBuilder(object):
 
     def setupNormal(self):
         self.aiNormal = cmds.shadingNode(
-            'aiNormalMap', name='aiNormalMap', asUtility=True)  # aiImage Node only visible in lit vp
+            'aiNormalMap', name='aiNormalMap', asUtility=True)
 
         cmds.setAttr(self.normalTex + '.filename',
                      self.pathNormal, type='string')
@@ -109,12 +133,33 @@ class matBuilder(object):
         cmds.connectAttr(self.aiNormal + '.outValue',
                          self.shader + '.normalCamera', force=True)
 
+    def setupDisp(self):
+        self.disp = cmds.shadingNode(
+            'displacementShader', name='displacementShader', asShader=True)
+
+        cmds.setAttr(self.dispTex + '.filename',
+                     self.pathDisplace, type='string')
+
+        cmds.setAttr(self.dispTex + '.colorSpace',
+                     'Utility - Raw', type='string')
+        cmds.connectAttr(self.dispTex + '.outColorR',
+                         self.disp + '.displacement', force=True)
+
+        cmds.connectAttr(self.disp + '.displacement',
+                         self.shadingGroup + '.displacementShader', force=True)
+        # TODO figure out how to connect displacement to shading group without object
+        # TODO setup displacement without shading group
+
 
 if __name__ == "__main__":
     newMat = matBuilder("ai_testMat")
     newMat.createShader()
+    newMat.createSphere()
+    newMat.assignShader()
     newMat.createFileNodes()
     newMat.setupDiff()
     newMat.setupMetal()
     newMat.setupRough()
     newMat.setupNormal()
+    print(cmds.listAttr(newMat.sphereShape))
+    newMat.setupDisp()
